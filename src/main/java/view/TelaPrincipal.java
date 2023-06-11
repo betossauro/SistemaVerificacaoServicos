f3f357b74423de2c4b940a54ce658e80610e95ab
Beto <pfvrbeto@gmail.com>
2023-06-11 19:56:27 -0300

    criação da tela principal com menu bar e vazia de menu onde todos os painéis serão chamados

--- src/main/java/view/TelaPrincipal.java
+++ src/main/java/view/TelaPrincipal.java
@@ -3,10 +3,23 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaPrincipal {
 
 	private JFrame frame;
+	private JMenuBar menuBar;
+	private JMenu mnCadastrar;
+	private JMenuItem mntmCadastrarUsuario;
+	private JMenu mnConsultar;
+	private JMenuItem mntmConsultarServico;
+	private JMenuItem mntmConsultarFuncionario;
+	private JMenu mnGerenciar;
+	private JMenuItem mntmGerenciarFuncionarios;
 
 	/**
 	 * Launch the application.
@@ -36,8 +49,48 @@ public class TelaPrincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1200, 1000);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		mnCadastrar = new JMenu("Cadastrar");
+		menuBar.add(mnCadastrar);
+		
+		mntmCadastrarUsuario = new JMenuItem("Cadastrar Usuário");
+		mntmCadastrarUsuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		mnCadastrar.add(mntmCadastrarUsuario);
+		
+		mnConsultar = new JMenu("Consultar");
+		menuBar.add(mnConsultar);
+		
+		mntmConsultarServico = new JMenuItem("Consultar Serviço");
+		mntmConsultarServico.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		mnConsultar.add(mntmConsultarServico);
+		
+		mntmConsultarFuncionario = new JMenuItem("Consultar Funcionário");
+		mntmConsultarFuncionario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		mnConsultar.add(mntmConsultarFuncionario);
+		
+		mnGerenciar = new JMenu("Gerenciar");
+		menuBar.add(mnGerenciar);
+		
+		mntmGerenciarFuncionarios = new JMenuItem("Gerenciar Funcionários");
+		mntmGerenciarFuncionarios.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		mnGerenciar.add(mntmGerenciarFuncionarios);
 	}
 
 }
