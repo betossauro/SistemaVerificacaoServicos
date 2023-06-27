0c93c87f05d2ed6f03305fe012ddf73b4715a250
barbaraLuersen <barbaraluersenlima@gmail.com>
2023-06-27 17:03:33 -0300

    Adição de teste de login com funcionario desativado

--- src/test/java/testeLogin/TesteLogin.java
+++ src/test/java/testeLogin/TesteLogin.java
@@ -16,6 +16,7 @@ class TesteLogin {
 
 	private static Funcionario gerente;
 	private static Funcionario funcionario;
+	private static Funcionario funcionarioDasativado;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -25,6 +26,10 @@ class TesteLogin {
 		funcionario = new Funcionario(7, "Bento Miguel Gustavo Aragão", "06556898937", "82991822653",
 				LocalDate.of(1944, 01, 02), "06556898937", "100006", "123456", TipoUsuario.FUNCIONARIO,
 				TipoCargo.ZELADORIA, null);
+
+		funcionarioDasativado = new Funcionario(4, "Joaquim José da Silva", "08725041632", "11999635214",
+				LocalDate.of(1995, 03, 05), "08725041632", "100002", "123456", TipoUsuario.FUNCIONARIO,
+				TipoCargo.FAXINA, LocalDate.of(2023, 06, 26));
 	}
 
 	@AfterAll
@@ -35,23 +40,35 @@ class TesteLogin {
 	void testeLoginGerenteValido() {
 		assertEquals("000001", gerente.getMatricula());
 		assertEquals("000011", gerente.getSenha());
+		assertEquals(null, gerente.getDataDesligamento());
 	}
 
 	@Test
 	void testeLoginGerenteInvalido() {
 		assertEquals("000001", gerente.getMatricula(), "MATRICULA INVALIDA");
 		assertEquals("000012", gerente.getSenha(), "SENHA INVÁLIDA");
+		assertEquals(null, gerente.getDataDesligamento());
 	}
 
 	@Test
 	void testeLoginFuncionarioValido() {
 		assertEquals("100006", funcionario.getMatricula());
 		assertEquals("123456", funcionario.getSenha());
+		assertEquals(null, funcionario.getDataDesligamento());
 	}
 
 	@Test
 	void testeLoginFuncionarioInvalido() {
 		assertEquals("1000015", funcionario.getMatricula(), "MATRICULA INVALIDA");
 		assertEquals("123456", funcionario.getSenha(), "SENHA INVÁLIDA");
+		assertEquals(null, funcionario.getDataDesligamento(), "USUARIO DESATIVADO");
 	}
+
+	@Test
+	void testeLoginFuncionarioDesativado() {
+		assertEquals("100002", funcionarioDasativado.getMatricula(), "MATRICULA INVALIDA");
+		assertEquals("123456", funcionarioDasativado.getSenha(), "SENHA INVÁLIDA");
+		assertEquals(null, funcionarioDasativado.getDataDesligamento(), "USUARIO DESATIVADO");
+	}
+
 }
