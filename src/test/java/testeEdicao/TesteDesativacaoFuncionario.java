d1c49920d84b75bac6699092d466750c0ab2a327
barbaraLuersen <barbaraluersenlima@gmail.com>
2023-06-27 16:41:24 -0300

    Merge branch 'beto' of https://github.com/betossauro/SistemaVerificacaoServicos into Barbara

--- src/test/java/testeEdicao/TesteDesativacaoFuncionario.java
+++ src/test/java/testeEdicao/TesteDesativacaoFuncionario.java
@@ -2,14 +2,24 @@ package testeEdicao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import model.vo.Funcionario;
+import model.vo.TipoCargo;
+import model.vo.TipoUsuario;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 class TesteDesativacaoFuncionario {
+
+	private static Funcionario funcionario;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		funcionario = new Funcionario(8, "Samuel Bruno Duarte", "04214228316", "92996317588",
+				LocalDate.of(1947, 02, 04), "04214228316", "100007", "123456", TipoUsuario.FUNCIONARIO,
+				TipoCargo.FAXINA, null);
 	}
 
 	@AfterAll
@@ -17,8 +27,15 @@ class TesteDesativacaoFuncionario {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testeDesativacaoFuncionario() {
+		funcionario.setDataDesligamento(LocalDate.of(2023, 06, 27));
+		assertNotEquals(null, funcionario.getDataDesligamento(), "FUNCIONÁRIO ATIVO!");
 	}
 
+	@Test
+	void testeFuncionarioAtivo() {
+		funcionario.setDataDesligamento(null);
+		assertEquals(LocalDate.of(2023, 06, 27), funcionario.getDataDesligamento(),
+				"O FUNCIONÁRIO NÃO PODE SER DESATIVADO POIS POSSUI UM SERVIÇO EM ANDAMENTO!");
+	}
 }
---
+++ src/test/java/testeLogin/TesteLogin.java
@@ -0,0 +1,57 @@
+package testeLogin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import model.vo.Funcionario;
+import model.vo.TipoCargo;
+import model.vo.TipoUsuario;
+
+class TesteLogin {
+
+	private static Funcionario gerente;
+	private static Funcionario funcionario;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		gerente = new Funcionario(1, "Luis Alberto", "10810699966", "48998577999", LocalDate.of(1999, 05, 19),
+				"10810699966", "000001", "000011", TipoUsuario.ADMINISTRADOR, TipoCargo.GERENCIA, null);
+
+		funcionario = new Funcionario(7, "Bento Miguel Gustavo Aragão", "06556898937", "82991822653",
+				LocalDate.of(1944, 01, 02), "06556898937", "100006", "123456", TipoUsuario.FUNCIONARIO,
+				TipoCargo.ZELADORIA, null);
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void testeLoginGerenteValido() {
+		assertEquals("000001", gerente.getMatricula());
+		assertEquals("000011", gerente.getSenha());
+	}
+
+	@Test
+	void testeLoginGerenteInvalido() {
+		assertEquals("000001", gerente.getMatricula(), "MATRICULA INVALIDA");
+		assertEquals("000012", gerente.getSenha(), "SENHA INVÁLIDA");
+	}
+
+	@Test
+	void testeLoginFuncionarioValido() {
+		assertEquals("100006", funcionario.getMatricula());
+		assertEquals("123456", funcionario.getSenha());
+	}
+
+	@Test
+	void testeLoginFuncionarioInvalido() {
+		assertEquals("1000015", funcionario.getMatricula(), "MATRICULA INVALIDA");
+		assertEquals("123456", funcionario.getSenha(), "SENHA INVÁLIDA");
+	}
+}
