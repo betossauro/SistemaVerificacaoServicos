0b257ac51b353bc2beeb677b181bd7183818b670
Beto <pfvrbeto@gmail.com>
2023-06-27 14:43:03 -0300

    base teste

--- src/test/java/testeCadastro/TelaCadastroDadosValidos.java
+++ src/test/java/testeCadastro/TelaCadastroDadosValidos.java
@@ -2,23 +2,43 @@ package testeCadastro;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import model.vo.Funcionario;
+import model.vo.TipoCargo;
+import model.vo.TipoUsuario;
+
 class TelaCadastroDadosValidos {
+	
+
+	private static Funcionario funcionarioAdm;
+	private static Funcionario funcionarioNormal;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		funcionarioAdm = new Funcionario(1, "Luis Alberto", "", null, null, null, null, null, null, null, null);
+		funcionarioNormal = new Funcionario(1, "Luis Alberto", "", null, null, null, null, null, null, null, null);
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 	}
+	
+	Funcionario funcionarioTeste(Integer id, String nome, String cpf, String telefone, LocalDate dataNascimento, String ctps,
+			String matricula, String senha, TipoUsuario tipoUsuario, TipoCargo tipoCargo, LocalDate dataDesligamento) {
+		Funcionario funcionarioTeste = new Funcionario();
+		return funcionarioTeste;
+	}
+	
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test() throws Exception {
+		Funcionario teste = funcionarioTeste(null, null, null, null, null, null, null, null, null, null, null);
+		assertEquals(funcionarioAdm.getTipoCargo(), teste.getTipoCargo());
 	}
 
 }
